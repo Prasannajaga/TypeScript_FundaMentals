7c529b3aa484a6ba38f750d3a592c2a3a1cdc909
prasanna <prasa_100@outlook.com>
2023-08-15 12:32:54 +0530

    indexSignatures covered

---
+++ indexSignatures.js
@@ -0,0 +1,10 @@
+var chores = {
+    sunday: "do the dishes",
+    monday: "walk the dog",
+    tuesday: "water the plants",
+    wednesday: "take out the trash",
+    thursday: "clean your room",
+    friday: "mow the lawn",
+    saturday: "Party vibes"
+};
+console.log(chores);
