67e939b6333555ada36f2f67f8186345073a5be5
prasanna <prasa_100@outlook.com>
2023-08-23 00:05:08 +0530

    utility types explained

---
+++ src/utilitiesType.ts
@@ -0,0 +1,67 @@
+
+
+export type GameModel =  { 
+    title: string,   
+    genre: string,
+    platform: string,
+    publisher: string, 
+    release_date: string, 
+  }
+ 
+
+  // Record<Keys,Types>
+
+  const record : Record<string, GameModel> = {
+        GTAV : {
+            title : "GTAV",
+            genre : "Action & Adventure",
+            platform : "PC",
+            release_date : '2013',
+            publisher : "Unreal"
+        }
+  }
+
+
+  // Pick<Types,Keys>
+ 
+  const pick : Pick<GameModel , "title" | "publisher"> = {
+    publisher : 'RockStar' , title : "GTAV"
+  }
+  
+
+  // Omit<Types,Keys>
+
+  const omit : Omit<GameModel , "release_date" | "platform" | "publisher"> = {
+     genre : "Action and Adventure" , title : "GTAV"
+  }
+
+
+  // Exclude<unionTypes,Keyswantedremove>
+ 
+  type myUnionType = "🍇" | "🍎" | "🫐" | "🍋"
+
+  const noLemonPlease : Exclude<myUnionType, "🍋" | ""> = "🍇";
+
+
+  // Extract<unionTypes,KeysYouWanted>
+
+  const tomatoMango : Extract<myUnionType, "🍋" |  "🍎"> = "🍋" || "🍎";
+ 
+
+  // Parameters<Function>
+
+  function getData(data1:string,data2:string,age:number,game:GameModel){}
+
+  const parameters : Parameters<typeof getData> = 
+  ["Prasanna" , "Jagadesh" , 21 , record['GTAV']]; 
+
+  // ConstructorParameters<Class>
+  
+  class Person {
+    constructor(game : GameModel) {}
+  }
+
+  const contructorParameters : ConstructorParameters<typeof Person> = [record["GTAV"]];
+
+  console.log(contructorParameters);
+  
